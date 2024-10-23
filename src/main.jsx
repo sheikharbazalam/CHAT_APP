54b3d6d6f905769afcfcfaf529619e4621eed818
sheikharbazalam <thespoof318@gmail.com>
2024-10-23 11:06:59 +0100

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
